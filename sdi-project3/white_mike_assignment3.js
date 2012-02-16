5f7fd730534b6c298be74670b9c3fcc6022d56b8
Mike White <mike@michaelwhitedesigns.com>
2012-02-15 21:39:22 -0700

    created json structure

--- sdi-project3/white_mike_assignment3.js
+++ sdi-project3/white_mike_assignment3.js
@@ -3,3 +3,39 @@
 // Assignment: Project 3
 
 alert("JavaScript works!");
+
+var 
+	zombieHunter = "Columbus",
+	numZombies = 5,
+	bodyParts = ["arm","leg","chest","head" ],
+	bullets = 50
+	bulletsPerZombie = [2, 4, 6, 8]
+;
+var zombieParts = function () {
+	var bodyPart = bodyParts[partNumber];
+		bulletsRemaining = bulletsPerZombie[partNumber]
+		console.log("He pulled out his shot gun and shot for the " + bodyPart + ". " + "Remembering the double tap rule he thought, there goes " + bulletsRemaining + " bullets");
+};
+var zombieDown = 1,
+	zombieToGo = numZombies - 1
+;
+var noZombies = true;
+
+
+// here's the story...
+console.log(
+zombieHunter + " walked out of the convenient store just in time to see " + numZombies +  " zombies coming toward him. But he could remember if he was supposed to aim for the " + bodyParts + "."
+);
+
+for (var partNumber = 0; partNumber < bodyParts.length; partNumber++) {
+	zombieParts()
+}
+
+while (numZombies > 0) {
+	console.log("There's " + zombieDown + " zombies down and " + zombieToGo + " zombies to go.");
+	numZombies--;
+	zombieDown++;
+	zombieToGo--;
+	
+};
+console.log("Take That! No more zombies.");
